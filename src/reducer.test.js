7542da3efd2e3221fad576537db60116581f9c1e
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-21 13:35:48 -0400

    reducer test passed

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -6,7 +6,7 @@ import { filterTodos } from "./actions";
 describe("CREATE_TODO", function () {
     test("creates a new todo", function () {
         const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
+        let state = reducer(initialState, createTodo("Test"));
         expect(state.todos).toHaveLength(1);
         expect(state.todos[0]).toEqual({id: 1, done: false, text: "Test"});
     })
